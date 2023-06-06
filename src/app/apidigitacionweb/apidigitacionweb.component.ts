37cc6a2f742f9d78cb0f0c34af4cf5788fd9dcbb
alejandro.perez.acosta@hotmail.com <alejandro.perez.acosta@hotmail.com>
2023-06-05 21:49:15 -0500

    version inicial

---
+++ src/app/apidigitacionweb/apidigitacionweb.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apidigitacionweb',
+  templateUrl: './apidigitacionweb.component.html',
+  styleUrls: ['./apidigitacionweb.component.scss']
+})
+export class ApidigitacionwebComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApidigitacionwebComponent } from './apidigitacionweb/apidigitacionweb.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApidigitacionwebComponent
   ],
   imports: [
     BrowserModule,
