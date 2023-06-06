a6efa5893643806ef9f6bd3a55caf3fd0135bdc8
alejandro.perez.acosta@hotmail.com <alejandro.perez.acosta@hotmail.com>
2023-06-05 22:02:52 -0500

    version inicial

---
+++ src/app/api-digitacion-service.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+//
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiDigitacionServiceService {
+  //
+  constructor() { }
+  /*
+  getLogRemoto() {
+    //
+    let url = 'http://localhost:81/api/Digitacion/ConsultarFormularioDinamico';
+    // 
+    return this.http.get<LogEntry[]>(url);   
+  }*/
+}
